8551108bce28f9c644330a949b7ea3ec4a1fc398
jack841021 <jack841021@outlook.com>
2016-11-08 22:17:38 +0800

    grader

--- smart.c
+++ smart.c
@@ -15,19 +15,19 @@ int grader(board)
     		    m = 4 - abs(n);
     		    if (j_ >= 0 and j_ <= 14 and board[i ][j_] != 99 and board[i ][j_] != 66)
     		    {
-                        board[i ][ j_] += m;
+                        board[i ][j_] += m;
     		    }
     		    if (i_ >= 0 and i_ <= 14 and board[i_][j_] != 99 and board[i_][j_] != 66)
     		    {
-    			board[i_][ j_] += m;
+    			board[i_][j_] += m;
     		    }
     		    if (i_ >= 0 and i_ <= 14 and board[i_][j ] != 99 and board[i_][j ] != 66)
     		    {
-    			board[i_][ j ] += m;
+    			board[i_][j ] += m;
     		    }
     		    if (_j >= 0 and _j <= 14 and board[i_][_j] != 99 and board[i_][_j] != 66)
     		    {
-    			board[i_][ _j] += m;
+    			board[i_][_j] += m;
     		    }
     		}
     	    }
@@ -41,19 +41,19 @@ int grader(board)
                     m = 4 - abs(n);
     		    if (j_ >= 0 and j_ <= 14 and board[i ][j_] != 99 and board[i ][j_] != 66)
     		    {
-                        board[i ][ j_] -= m;
+                        board[i ][j_] -= m;
     		    }
     		    if (i_ >= 0 and i_ <= 14 and board[i_][j_] != 99 and board[i_][j_] != 66)
     		    {
-                        board[i_][ j_] -= m;
+                        board[i_][j_] -= m;
     		    }
                     if (i_ >= 0 and i_ <= 14 and board[i_][j ] != 99 and board[i_][j ] != 66)
     		    {
-    			board[i_][ j ] -= m;
+    			board[i_][j ] -= m;
                     }
                     if (_j >= 0 and _j <= 14 and board[i_][_j] != 99 and board[i_][_j] != 66)
     		    {
-                        board[i_][ _j] -= m;
+                        board[i_][_j] -= m;
     		    }
     		}
     	    }
