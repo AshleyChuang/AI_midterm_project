a0960c6dd4c172b27e68a3590c315aa649126804
jack841021 <jack841021@outlook.com>
2016-11-11 07:37:14 +0800

    Update smart.c

--- smart.c
+++ smart.c
@@ -10,53 +10,53 @@ int grader(board)
                 for (n = -3, n <= 4, n += 1)
                 {
                     j_ = j + n;
-    		    i_ = i + n;
-    		    _j = j - n;
-    		    m = 4 - abs(n);
-    		    if (j_ >= 0 and j_ <= 14 and board[i ][j_] != 99 and board[i ][j_] != 66)
-    		    {
+                    i_ = i + n;
+                    _j = j - n;
+                    m = 4 - abs(n);
+                    if (j_ >= 0 and j_ <= 14 and board[i ][j_] != 99 and board[i ][j_] != 66)
+                    {
                         board[i ][j_] += m;
-    		    }
-    		    if (i_ >= 0 and i_ <= 14 and board[i_][j_] != 99 and board[i_][j_] != 66)
-    		    {
-    			board[i_][j_] += m;
-    		    }
-    		    if (i_ >= 0 and i_ <= 14 and board[i_][j ] != 99 and board[i_][j ] != 66)
-    		    {
-    			board[i_][j ] += m;
-    		    }
-    		    if (_j >= 0 and _j <= 14 and board[i_][_j] != 99 and board[i_][_j] != 66)
-    		    {
-    			board[i_][_j] += m;
-    		    }
-    		}
-    	    }
-    	    else if (board[i][j] == 66)
-    	    {
+                    }
+                    if (i_ >= 0 and i_ <= 14 and board[i_][j_] != 99 and board[i_][j_] != 66)
+                    {
+                        board[i_][j_] += m;
+                    }
+                    if (i_ >= 0 and i_ <= 14 and board[i_][j ] != 99 and board[i_][j ] != 66)
+                    {
+                        board[i_][j ] += m;
+                    }
+                    if (_j >= 0 and _j <= 14 and board[i_][_j] != 99 and board[i_][_j] != 66)
+                    {
+                        board[i_][_j] += m;
+                    }
+                }
+            }
+            else if (board[i][j] == 66)
+            {
                 for (n = -3, n <= 4, n += 1)
-    		{
+                {
                     j_ = j + n;
                     i_ = i + n;
                     _j = j - n;
                     m = 4 - abs(n);
-    		    if (j_ >= 0 and j_ <= 14 and board[i ][j_] != 99 and board[i ][j_] != 66)
-    		    {
+                    if (j_ >= 0 and j_ <= 14 and board[i ][j_] != 99 and board[i ][j_] != 66)
+                    {
                         board[i ][j_] -= m;
-    		    }
-    		    if (i_ >= 0 and i_ <= 14 and board[i_][j_] != 99 and board[i_][j_] != 66)
-    		    {
+                    }
+                    if (i_ >= 0 and i_ <= 14 and board[i_][j_] != 99 and board[i_][j_] != 66)
+                    {
                         board[i_][j_] -= m;
-    		    }
+                    }
                     if (i_ >= 0 and i_ <= 14 and board[i_][j ] != 99 and board[i_][j ] != 66)
-    		    {
-    			board[i_][j ] -= m;
+                    {
+                        board[i_][j ] -= m;
                     }
                     if (_j >= 0 and _j <= 14 and board[i_][_j] != 99 and board[i_][_j] != 66)
-    		    {
+                    {
                         board[i_][_j] -= m;
-    		    }
-    		}
-    	    }
-    	}
+                    }
+                }
+            }
+        }
     }
 }
