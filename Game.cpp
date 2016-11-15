ca26b5b9d8e83341036f0083ce01e7a5f26e0647
YiKiLI <919307612@qq.com>
2016-11-16 03:05:08 +0800

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -22,21 +22,6 @@ Game::Game(){
             chessboard[i][j] = BLANK;
 }
 
-int main()
-{
-    char k;
-    system("color E0");//设置颜色
-    do{
-        InitGame();
-        int player=1;
-        Game();
-        printf("Do you want a new turn?input y or n："); getchar(); scanf("%c",&k);
-        while(k!='y'&&k!='n'){ printf("INPUT ERROR!\n"); scanf("%c",&k); }
-        system("cls");
-    }while(k=='y');
-    printf("Thanks for using!\n");
-    return 0;
-}
 
 void Game:: MakePoint( Point * pPoint, int iGameFlag )
 {
