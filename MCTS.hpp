6c61ff8245888acc6801cc6f2bcc59440f9a8bdc
Jonathan Hsu <jack841021@outlook.com>
2016-11-17 23:21:50 +0800

    Update MCTS.hpp

--- MCTS.hpp
+++ MCTS.hpp
@@ -588,8 +588,6 @@ void multiplier(int player){
                 }
             } k = j;
         }if (c != 0) {
-            int a = i;
-            int b = k - c - 1;
             temp_board[i][k - c] *= c;
             c = 0;
         } else if (d != 0) {
